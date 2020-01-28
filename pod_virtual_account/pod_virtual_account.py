ce3870b9043264b19dd7b76135d278e42bf6269a
ReZa ZaRe <rz.zare@gmail.com>
2020-01-28 15:55:57 +0330

    release version 1.0.0

--- pod_virtual_account/pod_virtual_account.py
+++ pod_virtual_account/pod_virtual_account.py
@@ -3,7 +3,6 @@ import json
 from os import path
 from pod_base import PodBase, calc_offset, PodException, ConfigException, InvalidDataException
 
-
 try:
     from urllib.parse import urlencode
 except ImportError:
@@ -15,7 +14,7 @@ class PodVirtualAccount(PodBase):
     def __init__(self, api_token, token_issuer="1", server_type="sandbox", config_path=None,
                  sc_api_key="", sc_voucher_hash=None):
         here = path.abspath(path.dirname(__file__))
-        self._services_file_path = path.join(here, "services.ini")
+        self._services_file_path = path.join(here, "services.json")
         super(PodVirtualAccount, self).__init__(api_token, token_issuer, server_type, config_path, sc_api_key,
                                                 sc_voucher_hash, path.join(here, "json_schema.json"))
 
@@ -49,7 +48,7 @@ class PodVirtualAccount(PodBase):
         self._validate(kwargs, "addWithdrawRulePlan")
 
         return self._request.call(
-            sc_product_id=super(PodVirtualAccount, self)._get_sc_product_id("/nzh/biz/addWithdrawRulePlan"),
+            super(PodVirtualAccount, self)._get_sc_product_settings("/nzh/biz/addWithdrawRulePlan"),
             params=kwargs, headers=self._get_headers(), **kwargs)
 
     def withdraw_rule_plan_list(self, business_id, page=1, size=50, access_token=None, **kwargs):
@@ -71,9 +70,8 @@ class PodVirtualAccount(PodBase):
         if access_token is not None:
             headers["_token_"] = access_token
 
-        return self._request.call(
-            sc_product_id=super(PodVirtualAccount, self)._get_sc_product_id("/nzh/withdrawRulePlanList"),
-            params=kwargs, headers=headers, **kwargs)
+        return self._request.call(super(PodVirtualAccount, self)._get_sc_product_settings("/nzh/withdrawRulePlanList"),
+                                  params=kwargs, headers=headers, **kwargs)
 
     def get_link_issue_withdraw_rule_by_plan(self, business_id, plan_id=None, redirect_uri=None, call_uri=None):
         """
@@ -114,9 +112,8 @@ class PodVirtualAccount(PodBase):
 
         self._validate(kwargs, "withdrawRuleList")
 
-        return self._request.call(
-            sc_product_id=super(PodVirtualAccount, self)._get_sc_product_id("/nzh/biz/withdrawRuleList"),
-            params=kwargs, headers=self._get_headers(), **kwargs)
+        return self._request.call(super(PodVirtualAccount, self)._get_sc_product_settings("/nzh/biz/withdrawRuleList"),
+                                  params=kwargs, headers=self._get_headers(), **kwargs)
 
     def revoke_withdraw_rule(self, rule_id, **kwargs):
         """
@@ -130,8 +127,7 @@ class PodVirtualAccount(PodBase):
         self._validate(kwargs, "revokeWithdrawRule")
 
         return self._request.call(
-            sc_product_id=super(PodVirtualAccount, self)._get_sc_product_id("/nzh/biz/revokeWithdrawRule",
-                                                                            method_type="post"),
+            super(PodVirtualAccount, self)._get_sc_product_settings("/nzh/biz/revokeWithdrawRule", method_type="post"),
             params=kwargs, headers=self._get_headers(), **kwargs)
 
     def withdraw_rule_usage_report(self, rule_id, access_token, **kwargs):
@@ -152,5 +148,5 @@ class PodVirtualAccount(PodBase):
         headers["_token_"] = access_token
 
         return self._request.call(
-            sc_product_id=super(PodVirtualAccount, self)._get_sc_product_id("/nzh/withdrawRuleUsageReport"),
+            super(PodVirtualAccount, self)._get_sc_product_settings("/nzh/withdrawRuleUsageReport"),
             params=kwargs, headers=headers, **kwargs)
